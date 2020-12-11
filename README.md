cd9b2ffb349b7e58eed5754cf1088e7612f0450d
scmoreira <silvanamoreira85@outlook.com>
2020-12-11 13:00:28 +0100

    finished

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 An app to practice Redux
 
+<img width='700' height='400' src='./image.PNG' alt='screenShot'>
+
+
 ## Technologies
 
 - React
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
 import alertReducer from './alertReducer';
-import productsReducer from './productsReducer'
+import productsReducer from './productsReducer';
 
 export default combineReducers({
     products: productsReducer,
     alert: alertReducer
-})
+});
